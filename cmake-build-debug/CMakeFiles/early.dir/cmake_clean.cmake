d00958f8b3aa27975d1608543b76578759e9fe8d
Lyudmila06 <lyudmila29062@gmai.com>
2021-01-19 19:15:02 +1000

    add parser tree with grammatic

---
+++ cmake-build-debug/CMakeFiles/early.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/early.dir/early.cpp.obj"
+  "CMakeFiles/early.dir/early_run.cpp.obj"
+  "CMakeFiles/early.dir/lexical.cpp.obj"
+  "CMakeFiles/early.dir/main.cpp.obj"
+  "CMakeFiles/early.dir/parse_tree.cpp.obj"
+  "early.exe"
+  "early.exe.manifest"
+  "early.pdb"
+  "libearly.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/early.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ early_run.h
@@ -0,0 +1,19 @@
+//
+// Created by 1 on 14/11/2020.
+//
+
+#ifndef EARLY_EARLY_RUN_H
+#define EARLY_EARLY_RUN_H
+
+#include "early.h"
+#include <iostream>
+#include "lexical.h"
+#include "parse_tree.h"
+
+
+using namespace std;
+
+void early_run(lexical_analyzer la);
+
+
+#endif //EARLY_EARLY_RUN_H
